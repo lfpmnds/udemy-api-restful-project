7a3a8fa5c58cb9616aba3eb207b3787c619fd49e
Luiz Felipe <luizfelipe.m93@gmail.com>
2022-04-08 09:02:17 -0300

    Projeto criado

---
+++ src/main/java/com/lfpmnds/udemyapirestfulproject/UdemyApiRestfulProjectApplication.java
@@ -0,0 +1,13 @@
+package com.lfpmnds.udemyapirestfulproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyApiRestfulProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyApiRestfulProjectApplication.class, args);
+	}
+
+}

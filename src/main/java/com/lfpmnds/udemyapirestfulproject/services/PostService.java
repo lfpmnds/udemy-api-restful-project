997db5253a3c24a93cc17b260cbf10e6966dce1b
Luiz Felipe <luizfelipe.m93@gmail.com>
2022-04-12 09:29:15 -0300

    Obtendo um post por id

---
+++ src/main/java/com/lfpmnds/udemyapirestfulproject/services/PostService.java
@@ -0,0 +1,22 @@
+package com.lfpmnds.udemyapirestfulproject.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lfpmnds.udemyapirestfulproject.domain.Post;
+import com.lfpmnds.udemyapirestfulproject.repositories.PostRepository;
+import com.lfpmnds.udemyapirestfulproject.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repository;
+			
+	public Post findById(String id) {
+		Optional<Post> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}	
+}
